96338878b9395f198176b877e24ddc37fc632407
NOEL-code <junusong12@hanyang.ac.kr>
2024-02-20 13:47:27 +0900

    디자인 변경

--- front/src/Home/First.jsx
+++ front/src/Home/First.jsx
@@ -4,13 +4,15 @@ const First = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center h-full">
       <div className="flex flex-col justify-center gap-5 items-center text-center">
-        <div className="text-2xl font-extrabold m-20">티끌모아태산!</div>
+        <div className="font-mono text-4xl font-extrabold m-20 text-green-300	">
+          티끌 모아 태산!
+        </div>
         <img
           src="https://www.nizform.com/ByStoreFile/105073/preview/view_105073.jpg"
           className="mx-auto w-52 sm:w-96 rounded-full"
           alt="main"
         />
-        <div className="w-full flex flex-col md:flex-row gap-2">
+        <div className="w-full flex flex-col md:flex-row gap-3 m-20">
           <Link
             to="/login"
             className="mx-auto w-52 sm:w-96 md:mr-2 rounded bg-yellow-300 px-2 py-2 text-md font-semibold text-white shadow-sm hover:bg-lime-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
