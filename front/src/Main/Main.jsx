7c2a17e8e669ea8e2e44c17352bb0b3841250f26
김영석 <kimyoungseok15@gmail.com>
2024-02-23 14:25:53 +0900

    feat: 메인페이지 수정

--- front/src/Main/Main.jsx
+++ front/src/Main/Main.jsx
@@ -1,30 +1,12 @@
 import React from "react";
-import FundingProgress from "../Components/Funding/FundingProgress";
-import { Progress } from "flowbite-react";
-import { useNavigate } from "react-router-dom";
+import Friends from "../Components/Main/Friends";
+import WishLists from "../Components/Main/WishLists";
+
 const Main = () => {
-  const navigate = useNavigate();
   return (
-    <div className="h-[100vh]">
-      Main
-      <h1 className="text-[20px] mt-2">Funding Progress bar 예시</h1>
-      <FundingProgress
-        targetFundingAmount={2000}
-        currentFundingAmount={1500}
-        remainDays={3}
-        customWidth="w-[300px]"
-        customHeight="h-[100px]"
-        customProgressBarWidth="w-[280px]"
-      />
-      <FundingProgress
-        targetFundingAmount={21234}
-        currentFundingAmount={15003}
-        remainDays={1}
-        customWidth="w-[500px]"
-        customHeight="h-[200px]"
-        customProgressBarWidth="w-[480px]"
-      />
-      <button onClick={() => navigate("/funding/11111")}>펀딩하기</button>
+    <div className="flex">
+      <Friends />
+      <WishLists />
     </div>
   );
 };
