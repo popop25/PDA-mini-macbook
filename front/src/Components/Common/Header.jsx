6daa97fc65a5d08f814c65abeed1bd8741cbea06
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-02-23 11:25:00 +0900

    feat : tailwind css 설정 수정

--- front/src/Components/Common/Header.jsx
+++ front/src/Components/Common/Header.jsx
@@ -1,15 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+// import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
 import { useNavigate } from "react-router-dom";
+
+import { Button, Navbar } from "flowbite-react";
 
 export default function Header() {
   const navigate = useNavigate();
   return (
     <div>
-      <Navbar bg="light" data-bs-theme="light">
+      <Navbar fluid rounded>
+        <Navbar.Brand>
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            티끌
+          </span>
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          {/* <Button>Get started</Button> */}
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          {/* <Navbar.Link href="#" active>
+            Home
+          </Navbar.Link> */}
+          <Navbar.Link
+            onClick={() => {
+              navigate("/main");
+            }}
+          >
+            홈
+          </Navbar.Link>
+          <Navbar.Link
+            onClick={() => {
+              navigate("/product");
+            }}
+          >
+            상품
+          </Navbar.Link>
+          <Navbar.Link
+            onClick={() => {
+              navigate("/wish");
+            }}
+          >
+            위시리스트
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+
+      {/* <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home">로고</Navbar.Brand>
           <Nav className="me-auto">
@@ -36,7 +76,7 @@ export default function Header() {
             </Nav.Link>
           </Nav>
         </Container>
-      </Navbar>
+      </Navbar> */}
     </div>
   );
 }
