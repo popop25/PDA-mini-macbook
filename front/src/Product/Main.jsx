7023da61b912c6bf3e0b42a68260c8fe2e3cebaa
NOEL-code <junusong12@hanyang.ac.kr>
2024-02-23 14:17:59 +0900

    카드 디자인

--- front/src/Product/Main.jsx
+++ front/src/Product/Main.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const Product = () => {
   const [products, setProducts] = useState([]);
-  const baseUrl = "http://localhost:3001/api"; // URL에 오타 수정("http:" -> "http://")
+  const baseUrl = "http://localhost:3001/api/product"; // URL에 오타 수정("http:" -> "http://")
 
   // useEffect를 사용하여 컴포넌트 마운트 시 데이터를 가져옵니다.
   useEffect(() => {
@@ -12,34 +12,37 @@ const Product = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(baseUrl);
-        setProducts(response.data); // 응답으로 받은 데이터를 상태에 설정
+
+        setProducts(response.data.products);
       } catch (error) {
         console.error("There was an error!", error);
       }
     };
     fetchData();
-  }, [baseUrl]); // baseUrl이 변경될 때마다 함수를 다시 실행
+  }, []); // baseUrl이 변경될 때마다 함수를 다시 실행
 
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {products.map((el) => (
-        <Card
-          key={el.id}
-          className="max-w-sm"
-          imgAlt="Meaningful alt text for an image that is not purely decorative"
-          imgSrc={el.imageUrl}
-        >
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {el.brandName}
-          </h5>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {el.title}
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {el.price}
-          </p>
-        </Card>
-      ))}
+    <div className="container">
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4 place-items-center">
+        {products.map((el) => (
+          <Card
+            key={el._id}
+            className="max-w-sm"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            imgSrc={el.imageUrl}
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {el.brandName}
+            </h5>
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {el.title}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {el.price}
+            </p>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
