7ef62a2a42dc8d9ab56dbcf3b023b02df43fb8de
김영석 <kimyoungseok15@gmail.com>
2024-02-23 13:58:56 +0900

    feat: wishList 변경, 메인페이지 수정

--- front/src/WishList/Main.jsx
+++ front/src/WishList/Main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Friends from "../Components/Main/Friends";
+import WishLists from "../Components/Main/WishLists";
 
 const WishList = () => {
-  return <div>WishList</div>;
+  return (
+    <div className="flex">
+      <Friends />
+      <WishLists />
+    </div>
+  );
 };
 
 export default WishList;
