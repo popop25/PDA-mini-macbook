fd6f5c36b04372d8b0160fb24d1a8604637fb0bf
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-02-23 10:28:24 +0900

    feat : product detail page 추가

--- front/src/Product/Main.jsx
+++ front/src/Product/Main.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <div>
+      Product
+      <Link
+        to={"/product/11111"}
+        className="w-[200px] bg-black rounded-lg text-white"
+      >
+        상품 A 상세페이지 가기
+      </Link>
+    </div>
+  );
 };
 
 export default Product;
