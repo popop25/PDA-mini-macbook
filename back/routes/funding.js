e761e26ae9904b685218e8d6c3d6ff69001c72e0
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-02-26 00:02:03 +0900

    feat : funding api 추가

--- back/routes/funding.js
+++ back/routes/funding.js
@@ -1,13 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const Funding = require("../models/Funding");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("hi");
 });
 
+// funding ID로 조회
+router.get("/:fundingId", (req, res, next) => {
+  Funding.find({
+    _id: req.params.fundingId,
+  })
+    .populate("user")
+    .populate("product")
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// funding 추가
 router.post("/", (req, res, next) => {
-  res.send("hello");
+  Funding.create(req.body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post("/:fundingId", async (req, res, next) => {
+  const newTransaction = { userId: req.body.userId, amount: req.body.amount };
+  console.log(newTransaction);
+  //   res.send("hi");
+  const result = await Funding.find({
+    _id: req.params.fundingId,
+  });
+  console.log(result);
+  //   res.send("hu");
+  const updateResult = await Funding.updateOne(
+    {
+      _id: req.params.fundingId,
+    },
+    {
+      $push: {
+        transaction: newTransaction,
+      },
+    }
+  );
+  res.send(updateResult);
 });
 
 module.exports = router;
