8f7e252ab16825a0be63ce293a44fec898338eeb
NOEL-code <junusong12@hanyang.ac.kr>
2024-02-25 13:20:28 +0900

    제품 상세 페이지

--- front/src/Product/Detail/DetailDescription.jsx
+++ front/src/Product/Detail/DetailDescription.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import sanitizeHtml from "sanitize-html";
 
-const htmlCode =
-  '<div class="cu-video">\n<div class="cu-video-aspect-ratio"><iframe src="https://www.youtube.com/embed/J9c2UCbOXR4"></iframe></div>\n</div>\n\n<p><img src="https://st.kakaocdn.net/product/gift/editor/20220620083501_55cab91b4d3d45d682860aaf991add9d.jpg"/> <img src="https://st.kakaocdn.net/product/gift/editor/20210904173638_571afce919984719aae6e8f6b535c947.jpg"/></p>';
+const htmlCode = "ㅁㄴㅇㄹ";
 
 const cleanCode = sanitizeHtml(htmlCode, {
   allowedTags: sanitizeHtml.defaults.allowedTags.concat([
