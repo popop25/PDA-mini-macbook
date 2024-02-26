0170d3c46fb0fb598078fb7846d6475fa84a773c
Jimin <jimin4124.22@gmail.com>
2024-02-26 15:48:53 +0900

    feat: D-day 계산 수정

--- front/src/WishList/Main.jsx
+++ front/src/WishList/Main.jsx
@@ -3,15 +3,11 @@ import WishList from "../Components/WishList";
 import { Row, Col } from "react-bootstrap"
 import { fetchWishes } from "../Api/WishApi";
 import { fetchFundingDetail } from "../Api/Funding"
-// 로그인 정보 가져오기 (recoil 이용)
-import { useRecoilState } from "recoil";
-import { userInfoState } from "../stores/auth";
 
 const userInfo = sessionStorage.getItem('AUTH_USER')
 console.log("여깅겨이ㅕ기여기이", userInfo)
 
 export default function WishListPage() {
-  // const [userInfo, setUserInfo] = useRecoilState(userInfoState);  // recoil
   const [myWishList, setMyWishList] = useState([]);
   const [fundingData, setFundingData] = useState([]);
   // console.log("여기",userInfo)
@@ -20,18 +16,28 @@ export default function WishListPage() {
   const customHeight = "h-[80px]"
   const customProgressBarWidth = "w-[280px]"
   const birthDayDate = new Date(userInfo.birthDay);
+
   // 오늘 날짜
   const today = new Date();
   // 생일이 이미 지났다면 내년 생일로 설정
-  if (birthDayDate.getMonth() < today.getMonth() || (birthDayDate.getMonth() === today.getMonth() && birthDayDate.getDate() < today.getDate())) {
+  if (
+    birthDayDate.getMonth() < today.getMonth() ||
+    (birthDayDate.getMonth() === today.getMonth() && birthDayDate.getDate() < today.getDate())
+  ) {
     birthDayDate.setFullYear(today.getFullYear() + 1);
   } else {
     birthDayDate.setFullYear(today.getFullYear());
   }
 
   // 오늘로부터의 차이 계산
-  const remainingDays = Math.ceil((birthDayDate - today) / (1000 * 60 * 60 * 24));
-  
+  let remainingDays = Math.floor((birthDayDate - today) / (1000 * 60 * 60 * 24));
+
+  // 생일이 지났다면 내년 생일로 설정
+  if (remainingDays < 0) {
+    birthDayDate.setFullYear(today.getFullYear() + 1);
+    remainingDays = Math.floor((birthDayDate - today) / (1000 * 60 * 60 * 24));
+  }
+
   useEffect(() => {
       async function fetchData() {
           const data1 = await fetchWishes(JSON.parse(userInfo).phoneNumber)    // user phoneNumber 전달
@@ -43,22 +49,6 @@ export default function WishListPage() {
   }, [])
   
   console.log(myWishList)
-
-  // useEffect(() => {
-  //   async function fetchFundingData() {
-  //     const fundingDataPromises = myWishList.map(async (wish) => {
-  //       console.log("여기여기여기여기",wish)
-  //       const data2 = await fetchFundingDetail(wish.productId);
-  //       return data2;
-  //     });
-
-  //     const allFundingData = await Promise.all(fundingDataPromises);
-  //     setFundingData(allFundingData);
-  //   }
-  //   fetchFundingData();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [myWishList]);
-
 
   return (
     <div className='bg-gradient-to-r from-yellow-100 to-green-100'>
