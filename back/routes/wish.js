d6a4e3441b2bb5e687a661c4c280dc779b0a2691
김영석 <kimyoungseok15@gmail.com>
2024-02-26 14:11:04 +0900

    feat: 위시리스트 조회 시 유저의 펀딩객체들도 조회. 메인화면 위시리스트 버튼 완성

--- back/routes/wish.js
+++ back/routes/wish.js
@@ -12,17 +12,19 @@ router.get('/:phoneNumber', authenticate, async (req, res, next) => {
     try {
         const phoneNumber = req.params.phoneNumber;
         const user = await User.findOne({phoneNumber: phoneNumber}).populate('isWishList');
-
         if (!user) {
             return res.status(404).json({error: "User not found"});
         }
+
+        const fundings = await Funding.find({ user: user._id }); // 해당 유저의 모든 Funding 객체 가져오기
 
         res.json({
             userEmail: user.userEmail,
             nickName: user.nickName,
             phoneNumber: user.phoneNumber,
             birthDay: user.birthDay,
-            isWishList: user.isWishList
+            isWishList: user.isWishList,
+            fundings : fundings
         });
     } catch (error) {
         next(error);
--- front/src/Main/Main.jsx
+++ front/src/Main/Main.jsx
@@ -1,30 +1,48 @@
 import React, { useEffect, useState } from "react";
 import Friends from "../Components/Main/Friends";
 import WishLists from "../Components/Main/WishLists";
-import axios from "axios";
 import { fetchUserFriends } from "../Api/UserApi";
+import { fetchWishes } from "../Api/WishApi";
 
 const Main = () => {
   const [friends, setFriends] = useState([]);
-  const [wishLists, setWishLists] = useState([]);
+  const [wishList, setWishList] = useState([]);
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchFriends = async () => {
       try {
-        const friendsData = await fetchUserFriends();
-        setFriends(friendsData);
-        console.log(friendsData);
+        const friendList = await fetchUserFriends();
+        setFriends(friendList);
       } catch (error) {
         console.error("Error fetching user friends:", error);
       }
     };
 
-    fetchData();
+    fetchFriends();
   }, []);
+
+  useEffect(() => {
+    if (phoneNumber === "") {
+      return;
+    }
+    const fetchWishLists = async () => {
+      try {
+        const wishListData = await fetchWishes(phoneNumber);
+        setWishList(wishListData);
+        console.log(wishList);
+      } catch (error) {
+        console.error("Error fetching user friends:", error);
+      }
+    };
+
+    fetchWishLists();
+  }, [phoneNumber]);
+
   return (
-    <div className="flex">
-      <Friends friends={friends} />
-      <WishLists />
+    <div className="flex bg-gradient-to-r from-yellow-100 to-green-100">
+      <Friends friends={friends} setPhoneNumber={setPhoneNumber} />
+      <WishLists wishList={wishList.isWishList} fundings={wishList.fundings} />
     </div>
   );
 };
